419f129f9715034e832bb2d33e304ab73656409a
James Hu <hello@james.hu>
2014-05-27 09:17:23 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "axsuul@gmail.com"
 license          "Apache 2.0"
 description      "Provides recipes for installing monit and maintaining monit configurations."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 
 recipe "monit", "Installs monit"
 
